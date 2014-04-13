a2507b106be5cc378fe24a86e47b889916086664
Ob Okaiwele <obokaiwele@gmail.com>
2014-04-13 11:23:38 -0400

    Initialize repository with first commit

---
+++ download-zip-codes.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl
+use strict;      # forces variable declaration
+use warnings;    # uses more verbose output from program
+
+use autodie;     # die if problem reading or writing a file
+use LWP::Simple; # enables access to internet content
+
+$| = 1;          # prevent output buffering. Output is shown immediately
+
+my $url = 'http://www.allareacodes.com/area-code-list.htm';
+my $outputFile = 'area-codes.txt';
+
+###############################################################
+# get file and cache its contents locally
+###############################################################
+print "Downloading zip codes...";
+my $file = get($url);
+print "done\n";
+
+###############################################################
+# create new output file. Overwrite existing output file if any
+###############################################################
+open(OUTPUT, '>' . $outputFile) or die 'Unable to create output file - ' . $outputFile;
+
+###############################################################
+# parse file and find all zip codes
+###############################################################
+print "Parsing zip codes...";
+my $regex = '<a href="/[0-9]{3}">([0-9]{3})</a>';
+my @zipCodes = $file =~ m/$regex/g;
+print "done\n";
+
+###############################################################
+# save zip codes to file
+###############################################################
+print "Saving zip codes...";
+print OUTPUT join("\n", @zipCodes);
+close (OUTPUT);
+print "done\n";
